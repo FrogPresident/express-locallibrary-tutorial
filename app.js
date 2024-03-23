ddfe0f6d01035b0f5b85df3eec927e744b427d32
Frogboy69 <48415155+FrogPresident@users.noreply.github.com>
2024-03-23 14:17:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 //Set up mongoose connection
 
 var mongoose = require("mongoose");
-var mongoDB = 'mongodb+srv://xup61i6u6:a2550943@cluster0.pxwiurn.mongodb.net/?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://xup61i6u6:<password>@cluster0.pxwiurn.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
